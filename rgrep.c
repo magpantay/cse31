1b0fcf19b7a6cc5546eef52363231fbabfb807e9
Kyran Magpantay <magpantay.kyran@gmail.com>
2016-10-06 08:55:32 -0700

    Implemented base cases
    Base cases is just basic matching without any special characters

--- rgrep.c
+++ rgrep.c
@@ -7,8 +7,41 @@
  * the first char of partial_line.
  */
 int matches_leading(char *partial_line, char *pattern) {
-  // Implement if desire 
-  return 0;
+  int i = 0;
+	int j = 0;
+	while (pattern[i] != '\0')
+	{
+		if (partial_line[j] == '\0')
+		{
+			return 0; //nothing matches
+		}
+		else if ((pattern[i] == partial_line[j]) || ((pattern[i] == '.')&&(partial_line[j + 1] != '\0'))) //'.' acts like a match
+		{
+			i++; //next char of pattern
+			j++; //next char of partial_line
+		}
+		else if (pattern[i] == '+')
+		{
+			char characterBeforePlus = pattern [i - 1];
+			while(characterBeforePlus == partial_line[j])
+			{
+					j++;
+			}	
+			i++;
+		}
+		
+		else if (pattern[i] != partial_line[j])
+		{
+			i = 0; //reset pattern to start at 0
+			j++; //but still traverse partial_line
+		}
+		else
+		{
+			return 0; //nothing matches
+		}
+	}
+
+   return 1;
 }
 
 /**
@@ -18,8 +51,9 @@ int matches_leading(char *partial_line, char *pattern) {
  * Implementation of the rgrep matcher function
  */
 int rgrep_matches(char *line, char *pattern) {
-  //implement
-  return 0;
+  int return_value = matches_leading(line, pattern);
+  
+  return return_value;
 }
 
 int main(int argc, char **argv) {
