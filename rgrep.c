386740e05b2a1f91dbabf77fd3d8205409aced50
Kyran Magpantay <magpantay.kyran@gmail.com>
2016-10-06 10:49:23 -0700

    Added special char checks in rgrep_matches, implemented '?'
    + I added checks to rgrep_matches, checking if there is '?' or '\' before pushing the pattern string to matches_leading. If there was a '?' or a '\' then there would be a couple more things to do before pushing the pattern string to matches_leading. 
    
    + How I implemented '?' is that since the character before '?' may appear one or more times, I made two substrings out of the pattern string: one with the character before the question mark, and one without the character before the question mark. From there, I'd push both of those (named string_without_char and string_with_char) into matches_leading, putting the result into the return_without_char value and the return_with_char value respectively. From there, I'd do a boolean check and if one of them or both of them returned a 1, then change the return_value to be 1. 
    
    + I also attempted to implement '\' with some failure. In other words, I need to work on '\' for it to work properly.
    
    + Status so far after testing:
       + base cases - Working
       + . (period) - Working 
       + + (plus) - Working, need to do more testing to verify
       + ? (question mark) - Working, need to do more testing to verify
       + \ (backslash) - Not working
       + Multiple special characters - Working as far as I know for everything except '\', need to do more testing to verify

--- rgrep.c
+++ rgrep.c
@@ -50,9 +50,109 @@ int matches_leading(char *partial_line, char *pattern) {
  * Implementation of the rgrep matcher function
  */
 int rgrep_matches(char *line, char *pattern) {
+	int i = 0;
+	int boolean0 = 1;
+	int return_value = 0; //because make was warning that I didn't return a value in the end, so I just made a return_value that equals 0 unless changed to 1 by a match from matches_leading
+	while (pattern[i] != '\0') //traverse pattern to see if there are special characters (except for '.' and '+')
+	{
+		if ((pattern[i] == '?')	|| (pattern[i] == '\\'))
+		{
+			boolean0 = 0;
+		}
+		i++;
+	}
 	
-	int return_value = 0; //because I need to return a value eventually
-	return_value = matches_leading(line,pattern);
+	if (boolean0 == 1) //if there are no special characters after traversing the pattern (except for '.' and '+')
+	{
+		return_value = matches_leading(line, pattern); //call matches_leading
+	}
+	
+	else //now if boolean0 == 0, reset and trace down symbols
+	{
+		boolean0 = 1;
+		i = 0;
+		while (pattern[i] != '\0') //check for '\\' now that we know there's symbols (other than '.' and '+')
+		{
+			if (pattern[i] == '\\')
+			{
+				boolean0 = 0;
+			}
+			i++;
+		}
+		if (boolean0 == 1) //do questionMark
+		{
+			char string_without_char[strlen(pattern) - 2]; //= [(strlen(pattern) - 2)];
+			char string_with_char[strlen(pattern) - 1];// = [(strlen(pattern) - 1)];
+	
+			int traverse_1 = 0;
+			int traverse_2 = 0;
+			
+			while (pattern[traverse_1] != '\0') //with the character
+			{
+				if (pattern [traverse_1] == '?') //doesn't fully work, but 80% works nonetheless (I'm guessing because the slash needs to be before all?)
+				{
+					traverse_1++;
+				}
+				string_with_char [traverse_2] = pattern [traverse_1];
+	
+				traverse_1++;
+				traverse_2++;
+			}
+			
+			traverse_1 = 0;
+			traverse_2 = 0;
+			
+			while (pattern[traverse_1] != '\0') //without the character
+			{
+				if (pattern[traverse_1 + 1] == '?') //if the next character over is a question mark
+				{
+					traverse_1 += 2; //skip that character and the question mark
+				}
+				string_without_char[traverse_2] = pattern[traverse_1];
+				traverse_1++;
+				traverse_2++;
+			}
+			
+			/* because calling the matches_leading function within an if statement like 
+			   if ((matches_leading(string_without_char)) || (matches_leading (string_with_char)))
+			   made question mark only work partially, just separated calling matches_leading into two boolean ints
+			   as seen below
+		  	*/
+		   
+			int return_with_char = 0; 
+			int return_without_char = 0;
+			return_without_char = matches_leading(line, string_without_char);
+			return_with_char = matches_leading(line, string_with_char);
+			
+			if (return_without_char || return_with_char)
+			{
+				return_value = 1;
+			}
+		}
+		
+		else //now, by deductive reasoning, we have '\\' left (need to work on)
+		{
+			char string_without_backslash[strlen(pattern) - 1];
+			int j = 0;
+			int k = 0;
+			while(pattern[j] != '\0')
+			{
+				if (pattern[j] == '\\') 
+				{
+					j++;
+				}
+				else
+				{
+					string_without_backslash[j] == pattern [k];
+					j++;
+					k++;
+				}
+				
+			}
+			return_value = (matches_leading(line, string_without_backslash));
+		}
+	}
+	
 	return return_value;
 }
 
