8e6b3315f67932da9c410b8faf6d4e553ab72dd4
Kyran Magpantay <magpantay.kyran@gmail.com>
2016-10-06 08:52:27 -0700

    Created rgrep.c
    Starting the project, this is the "skeleton" file we get.
    
    The project implementation is grep, but restrictive with a set of wildcard characters that can be within the pattern (the search term).
    
    . (period) Matches any character.
    + (plus) The preceding character will appear one or more times
    ? (question) The preceding character may or may not appear in the line
    \ (backslash) "Escapes" the following character, nullifying any special characteristics

---
+++ rgrep.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define MAXSIZE 4096
+
+/**
+ * You can use this recommended helper function 
+ * Returns true if partial_line matches pattern, starting from
+ * the first char of partial_line.
+ */
+int matches_leading(char *partial_line, char *pattern) {
+  // Implement if desire 
+  return 0;
+}
+
+/**
+ * You may assume that all strings are properly null terminated 
+ * and will not overrun the buffer set by MAXSIZE 
+ *
+ * Implementation of the rgrep matcher function
+ */
+int rgrep_matches(char *line, char *pattern) {
+  //implement
+  return 0;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <PATTERN>\n", argv[0]);
+        return 2;
+    }
+
+    /* we're not going to worry about long lines */
+    char buf[MAXSIZE];
+
+    while (!feof(stdin) && !ferror(stdin)) {
+        if (!fgets(buf, sizeof(buf), stdin)) {
+            break;
+        }
+        if (rgrep_matches(buf, argv[1])) {
+            fputs(buf, stdout);
+            fflush(stdout);
+        }
+    }
+
+    if (ferror(stdin)) {
+        perror(argv[0]);
+        return 1;
+    }
+
+    return 0;
+}
