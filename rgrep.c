fe31b1f8c1f11e68496dd81fdaf103843d55bb55
Kyran Magpantay <magpantay.kyran@gmail.com>
2016-10-11 16:51:03 -0700

    Major update: Logix fixes, improved & added functionality
    Changes from previous update:
    + Added the \ case and it should be working for most if not all cases (including \[special case character like '.'])
        + Please check comments in code to see how I did it
    + Changed a couple of logic errors found in the previous version
    + Improved the + functionality, especially when the character before '+' is '.', such as in w.+
    + Renamed some variables for added readability of code
    + Removed some unnecessary variables, especially global variables
    + Added comments to add readability to code/make it easier to follow/show my thinking process in the case something isn't working
    + Removed #include<assert.h> as it is unnecessary to include at this point
    
    Known bugs/issues:
    - '\?' doesn't fully work, may need to check logic in '?' clauses
    - '\' works (like ab\\cd returns ab\cd, even ab\\\cd returns ab\cd [normal behavior]), except for multiple consecutive \ that are > 3 (like ab\\\\cd will not return ab\\cd)
    - '+' doesn't fully work, may need to check if statements regarding comparisons of pattern_length and line_length (ex. a+ah returns aah [normal behavior], but a+a+h does not return aah)

--- rgrep.c
+++ rgrep.c
@@ -1,33 +1,124 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #define MAXSIZE 4096
 
 /**
- * You can use this recommended helper function 
+ * You can use this recommended helper function
  * Returns true if partial_line matches pattern, starting from
  * the first char of partial_line.
  */
+  
 int matches_leading(char *partial_line, char *pattern) {
 	int i = 0;
 	int j = 0;
+	int plusCount = 0;
 	while (pattern[i] != '\0')
 	{
 		if (partial_line[j] == '\0')
 		{
 			return 0; //nothing matches
 		}
-		else if ((pattern[i] == partial_line[j]) || ((pattern[i] == '.')&&(partial_line[j + 1] != '\0'))) //'.' acts like a match
+		else if (partial_line[j] == '\n')
+		{
+			j++; //next line (since it would be a newline)
+		}
+		else if (pattern[i] == '?' && pattern[i-1] == '\\')
+		{
+			if (pattern[i] == partial_line[j])
+			{
+				i++; //next char of pattern
+				j++; //next char of partial_line
+			}
+			else
+			{
+				i = 0; //reset pattern to start at 0
+				j++; //but still traverse partial_line
+			}
+		}
+		else if (pattern[i] == '.' && pattern[i-1] == '\\')
+		{
+			if (pattern[i] == partial_line[j])
+			{
+				i++; //next char of pattern
+				j++; //next char of partial_line
+			}
+			else
+			{
+				i = 0; //reset pattern to start at 0
+				j++; //but still traverse partial_line
+			}
+		}
+		else if (pattern [i] == '+' && pattern[i-1] == '\\')
+		{
+			if (pattern[i] == partial_line[j])
+			{
+				i++; //next char of pattern
+				j++; //next char of partial_line
+			}
+			else
+			{
+				i = 0; //reset pattern to start at 0
+				j++; //but still traverse partial_line
+			}
+		}
+		else if (pattern[i+1] == '\\' && pattern[i] == '\\')
+		{
+			if (pattern[i+1] == partial_line[j])
+			{
+				i+=2; //next char of pattern
+				j++; //next char of partial_line
+			}
+			else
+			{
+				i = 0; //reset pattern to start at 0
+				j++; //but still traverse partial_line
+			}
+		}
+		else if (pattern[i] == '\\' && (pattern[i+1] != '+' || pattern[i+1] != '.' || pattern[i+1] != '?' || pattern[i+1] != '\\'))
+		{
+			if (pattern[i+1] == partial_line[j])
+			{
+				i+=2;
+				j++;
+			}
+			else
+			{
+				i = 0;
+				j++;
+			}
+		}
+		else if ((pattern[i] == '+') && (pattern[i-1] != '\\'))
+		{
+			plusCount++;
+			char characterBeforePlus = pattern [i-1];
+			if (characterBeforePlus == '.') //now if the character before plus is a dot (ex. w.+h)
+			{
+				characterBeforePlus = partial_line[j]; //make character before plus equal the current character of partial_line
+			} 	//necessary for the special case of dot, because if it didn't do this, it would literally match dot to whatever partial_line
+			
+			while(characterBeforePlus == partial_line[j])
+			{
+					j++; //keep going while it's a match
+			}	
+			i++; //increment pattern to go to the next character
+			int pattern_length = strlen(pattern); //get the lengths
+			int line_length = strlen(partial_line); //of the strings
+			
+			while ((pattern[i] == characterBeforePlus) && ((pattern_length - plusCount) < (line_length)))
+			{
+				i++; //"ignore" the next character if the next character after + matches the character before plus, but check the string lengths so it doesn't print out any non-matches
+			}	
+
+		} 
+		else if ((pattern[i] == partial_line[j]) || (pattern[i] == '.' && partial_line[j+1] != '\0' && pattern[i-1] != '\\')) //'.' acts like a match and checks ahead for a null terminator
 		{
 			i++; //next char of pattern
 			j++; //next char of partial_line
 		}
-		else if (pattern[i] == '+')
+		else if (pattern[i] == '\\')
 		{
-			char characterBeforePlus = pattern [i - 1];
-			while(characterBeforePlus == partial_line[j])
-			{
-					j++;
-			}	
-			i++;
+			i++; //here because all of my checks for \[symbol] have it so if the previous character is a '\\'
 		}
 		else if (pattern[i] != partial_line[j])
 		{
@@ -40,48 +131,38 @@ int matches_leading(char *partial_line, char *pattern) {
 		}
 	}
 
-	return 1;
+   return 1;
 }
-
 /**
- * You may assume that all strings are properly null terminated 
- * and will not overrun the buffer set by MAXSIZE 
+ * You may assume that all strings are properly null terminated
+ * and will not overrun the buffer set by MAXSIZE
  *
  * Implementation of the rgrep matcher function
  */
 int rgrep_matches(char *line, char *pattern) {
 	int i = 0;
-	int boolean0 = 1;
+	int symbolCheck = 1; //checks if there are certain symbols ('\\' and '?') before calling matches_leading 
 	int return_value = 0; //because make was warning that I didn't return a value in the end, so I just made a return_value that equals 0 unless changed to 1 by a match from matches_leading
 	while (pattern[i] != '\0') //traverse pattern to see if there are special characters (except for '.' and '+')
 	{
 		if ((pattern[i] == '?')	|| (pattern[i] == '\\'))
 		{
-			boolean0 = 0;
+			symbolCheck = 0;
 		}
 		i++;
 	}
 	
-	if (boolean0 == 1) //if there are no special characters after traversing the pattern (except for '.' and '+')
+	if (symbolCheck == 1) //if there are no special characters after traversing the pattern (except for '.' and '+')
 	{
 		return_value = matches_leading(line, pattern); //call matches_leading
 	}
 	
-	else //now if boolean0 == 0, reset and trace down symbols
+	else //now if symbolCheck == 0, reset and trace down symbols
 	{
-		boolean0 = 1;
 		i = 0;
-		while (pattern[i] != '\0') //check for '\\' now that we know there's symbols (other than '.' and '+')
+		if (pattern[i] == '?' || pattern[i-1] != '\\') //flawed logic works 
 		{
-			if (pattern[i] == '\\')
-			{
-				boolean0 = 0;
-			}
-			i++;
-		}
-		if (boolean0 == 1) //do questionMark
-		{
-			char string_without_char[strlen(pattern) - 2];
+			char string_without_char[strlen(pattern) - 2]; 
 			char string_with_char[strlen(pattern) - 1];
 	
 			int traverse_1 = 0;
@@ -89,7 +170,7 @@ int rgrep_matches(char *line, char *pattern) {
 			
 			while (pattern[traverse_1] != '\0') //with the character
 			{
-				if (pattern [traverse_1] == '?')
+				if (pattern [traverse_1] == '?') 
 				{
 					traverse_1++;
 				}
@@ -117,7 +198,7 @@ int rgrep_matches(char *line, char *pattern) {
 			   if ((matches_leading(string_without_char)) || (matches_leading (string_with_char)))
 			   made question mark only work partially, just separated calling matches_leading into two boolean ints
 			   as seen below
-		  	*/
+		   */
 		   
 			int return_with_char = 0; 
 			int return_without_char = 0;
@@ -129,32 +210,13 @@ int rgrep_matches(char *line, char *pattern) {
 				return_value = 1;
 			}
 		}
-		
-		else //now, by deductive reasoning, we have '\\' left (need to work on)
+		if (pattern[i] == '?' && pattern[i-1] == '\\')
 		{
-			char string_without_backslash[strlen(pattern) - 1];
-			int j = 0;
-			int k = 0;
-			while(pattern[j] != '\0')
-			{
-				if (pattern[j] == '\\') 
-				{
-					j++;
-				}
-				else
-				{
-					string_without_backslash[j] == pattern [k];
-					j++;
-					k++;
-				}
-				
-			}
-			return_value = (matches_leading(line, string_without_backslash));
+			matches_leading(line,pattern);
 		}
 	}
-	
 	return return_value;
-}
+} 
 
 int main(int argc, char **argv) {
     if (argc != 2) {
